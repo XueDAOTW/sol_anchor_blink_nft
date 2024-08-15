46f0809c8b93722b0886dc80c5f9d37151e6c331
cheng-chun-yuan <albert2367593@gmail.com>
2024-08-15 13:46:19 +0800

    feat(web): finish action and blink

--- app/my-app/next.config.mjs
+++ app/my-app/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    webpack: (config, options) => {
+        if (options.nextRuntime === 'edge') {
+          config.resolve.fallback = {
+            ...config.resolve.fallback,
+            crypto: 'crypto-browserify',
+            stream: 'stream-browserify',
+          };
+        }
+        return config;
+    },
+  };
+  
 export default nextConfig;
